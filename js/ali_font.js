aa9d67836df24181dbff7bc6bc1ef8c53cd7efcd
Stare <2504427864@qq.com>
2023-03-01 19:24:29 +0800

    Site updated: 2023-03-01 19:24:03

---
+++ js/ali_font.js
@@ -0,0 +1,5 @@
+# https://www.npmjs.com/package/hexo-butterfly-tag-plugins-plus
+npm install hexo-butterfly-tag-plugins-plus --save
+
+npm uninstall hexo-renderer-marked --save
+npm install hexo-renderer-kramed --save
